2a7141b962371b5b8cefd510ac0a4f5eaac1d70a
Tyler Tran <77897545+tylerttrannn@users.noreply.github.com>
2026-03-27 00:04:49 -0700

    merged with new peterworks page

--- asme-website/src/pages/Peterworks.tsx
+++ asme-website/src/pages/Peterworks.tsx
@@ -40,7 +40,7 @@ function Peterworks(){
                             </div>
                         </div>
                         <div className = "aspectRatio w-full">
-                            <iframe className="w-full h-full" src="https://www.youtube.com/embed/OiqO-Q6URTA?si=MGsnomD-Pi9cKGBI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            <iframe className="w-full h-full" src="https://www.youtube.com/embed/OiqO-Q6URTA?si=MGsnomD-Pi9cKGBI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                         </div>
                     </div>
                 </div>
