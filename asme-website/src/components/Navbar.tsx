200759c209eb42a85b8822fb9a0d06aa0ef885eb
brontondieu <brontondieu1@gmail.com>
2026-07-05 14:44:28 -0700

    navbar font changes

--- asme-website/src/components/Navbar.tsx
+++ asme-website/src/components/Navbar.tsx
@@ -6,14 +6,14 @@ function Navbar() {
   return (
     <div className="hidden md:block">
       <Reveal>
-        <div className="font-helevtica flex justify-center p-4">
+        <div className="font-joyful flex justify-center p-4">
           <NavigationMenu className="bg-white/90 backdrop-blur-sm px-6 py-1 rounded-full border border-gray-100 shadow-sm">
             <NavigationMenuList className="gap-1">
               
               {/* Home Link */}
               <NavigationMenuItem>
-                <NavigationMenuLink asChild className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-gray-100 focus:bg-gray-100 rounded-full transition-colors`}>
-                  <Link to="/" className="text-sm font-normal text-gray-800">Home</Link>
+                <NavigationMenuLink asChild className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-gray-200 focus:bg-gray-100 rounded-full transition-colors`}>
+                  <Link to="/" className="text-lg font-joyful text-gray-800">Home</Link>
                 </NavigationMenuLink>
               </NavigationMenuItem>
 
@@ -22,9 +22,9 @@ function Navbar() {
                 <NavigationMenuItem key={label}>
                   <NavigationMenuLink 
                     asChild 
-                    className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-gray-100 focus:bg-gray-100 rounded-full transition-colors`}
+                    className={`${navigationMenuTriggerStyle()} bg-transparent hover:bg-gray-200 focus:bg-gray-100 rounded-full transition-colors`}
                   >
-                    <Link to={`/${label.toLowerCase()}`} className="text-sm font-normal text-gray-800">
+                    <Link to={`/${label.toLowerCase()}`} className="text-lg font-joyful text-gray-800">
                       {label}
                     </Link>
                   </NavigationMenuLink>
@@ -33,28 +33,28 @@ function Navbar() {
 
               {/* About Link w/ dropdown */}
               <NavigationMenuItem>
-                <NavigationMenuTrigger className="justify-end ml-auto bg-transparent hover:bg-gray-100 data-[state=open]:bg-gray-100 rounded-full font-normal text-sm text-gray-800 transition-colors">
+                <NavigationMenuTrigger className="justify-end ml-auto bg-transparent hover:bg-gray-200 data-[state=open]:bg-gray-100 rounded-full font-joyful text-lg text-gray-800 transition-colors">
                   More
                 </NavigationMenuTrigger>
                 <NavigationMenuContent className="">
                   <ul className="grid w-[180px] gap-1 p-2 bg-white rounded-xl ">
                     <li>
                       <NavigationMenuLink asChild className = "">
-                        <Link to="/peterworks" className="block p-2 text-sm hover:bg-gray-100 rounded-md transition-colors text-gray-700 ">
+                        <Link to="/peterworks" className="block p-2 text-lg hover:bg-gray-200 rounded-md transition-colors text-gray-700 ">
                           Peterworks
                         </Link>
                       </NavigationMenuLink>
                     </li>
                     <li>
                       <NavigationMenuLink asChild>
-                        <Link to="/board" className="block p-2 text-sm hover:bg-gray-100 rounded-md transition-colors text-gray-700">
+                        <Link to="/board" className="block p-2 text-lg hover:bg-gray-200 rounded-md transition-colors text-gray-700">
                           Board
                         </Link>
                       </NavigationMenuLink>
                     </li>
                     <li>
                       <NavigationMenuLink asChild>
-                        <Link to="/sponsors" className="block p-2 text-sm hover:bg-gray-100 rounded-md transition-colors text-gray-700">
+                        <Link to="/sponsors" className="block p-2 text-lg hover:bg-gray-200 rounded-md transition-colors text-gray-700">
                           Sponsors
                         </Link>
                       </NavigationMenuLink>
